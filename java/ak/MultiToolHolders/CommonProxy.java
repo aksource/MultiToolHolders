79f44edd784b298a4fa526fd7b893adf24157d13
aksource <akira.mew@gmail.com>
2014-02-24 21:36:13 +0900

    Change repository management

---
+++ java/ak/MultiToolHolders/CommonProxy.java
@@ -0,0 +1,121 @@
+package ak.MultiToolHolders;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import ak.MultiToolHolders.Client.GuiToolHolder;
+import cpw.mods.fml.common.network.IGuiHandler;
+
+public class CommonProxy implements IGuiHandler
+{
+	public CommonProxy(){}
+	public void registerClientInformation(){}
+
+	public void registerTileEntitySpecialRenderer(){}
+
+	//returns an instance of the Container you made earlier
+	@Override
+	public Object getServerGuiElement(int id, EntityPlayer player, World world, int x, int y, int z) {
+		if(id == MultiToolHolders.guiIdHolder3)
+		{
+			if(player.getCurrentEquippedItem().getItem() instanceof ItemMultiToolHolder)
+			{
+				ItemStack stack = player.getCurrentEquippedItem();
+                InventoryToolHolder inventoryToolHolder = new InventoryToolHolder(stack, world);
+				return new ContainerToolHolder(player.inventory, inventoryToolHolder, 3);
+			}
+			else
+				return null;
+		}
+		else if(id == MultiToolHolders.guiIdHolder5)
+		{
+			if(player.getCurrentEquippedItem().getItem() instanceof ItemMultiToolHolder)
+			{
+				ItemStack stack = player.getCurrentEquippedItem();
+                InventoryToolHolder inventoryToolHolder = new InventoryToolHolder(stack, world);
+				return new ContainerToolHolder(player.inventory, inventoryToolHolder, 5);
+			}
+			else
+				return null;
+		}
+		else if(id == MultiToolHolders.guiIdHolder7)
+		{
+			if(player.getCurrentEquippedItem().getItem() instanceof ItemMultiToolHolder)
+			{
+				ItemStack stack = player.getCurrentEquippedItem();
+                InventoryToolHolder inventoryToolHolder = new InventoryToolHolder(stack, world);
+				return new ContainerToolHolder(player.inventory, inventoryToolHolder, 7);
+			}
+			else
+				return null;
+		}
+		else if(id == MultiToolHolders.guiIdHolder9)
+		{
+			if(player.getCurrentEquippedItem().getItem() instanceof ItemMultiToolHolder)
+			{
+				ItemStack stack = player.getCurrentEquippedItem();
+                InventoryToolHolder inventoryToolHolder = new InventoryToolHolder(stack, world);
+				return new ContainerToolHolder(player.inventory, inventoryToolHolder, 9);
+			}
+			else
+				return null;
+		}
+		else
+			return null;
+	}
+
+	//returns an instance of the Gui you made earlier
+	@Override
+	public Object getClientGuiElement(int id, EntityPlayer player, World world, int x, int y, int z) {
+		if(id == MultiToolHolders.guiIdHolder3)
+		{
+			if(player.getCurrentEquippedItem().getItem() instanceof ItemMultiToolHolder)
+			{
+				ItemStack stack = player.getCurrentEquippedItem();
+                InventoryToolHolder inventoryToolHolder = new InventoryToolHolder(stack, world);
+				return new GuiToolHolder(player.inventory, inventoryToolHolder, 3);
+			}
+			else
+				return null;
+		}
+		else if(id == MultiToolHolders.guiIdHolder5)
+		{
+			if(player.getCurrentEquippedItem().getItem() instanceof ItemMultiToolHolder)
+			{
+				ItemStack stack = player.getCurrentEquippedItem();
+                InventoryToolHolder inventoryToolHolder = new InventoryToolHolder(stack, world);
+				return new GuiToolHolder(player.inventory, inventoryToolHolder, 5);
+			}
+			else
+				return null;
+		}
+		else if(id == MultiToolHolders.guiIdHolder7)
+		{
+			if(player.getCurrentEquippedItem().getItem() instanceof ItemMultiToolHolder)
+			{
+				ItemStack stack = player.getCurrentEquippedItem();
+                InventoryToolHolder inventoryToolHolder = new InventoryToolHolder(stack, world);
+				return new GuiToolHolder(player.inventory, inventoryToolHolder, 7);
+			}
+			else
+				return null;
+		}
+		else if(id == MultiToolHolders.guiIdHolder9)
+		{
+			if(player.getCurrentEquippedItem().getItem() instanceof ItemMultiToolHolder)
+			{
+				ItemStack stack = player.getCurrentEquippedItem();
+                InventoryToolHolder inventoryToolHolder = new InventoryToolHolder(stack, world);
+				return new GuiToolHolder(player.inventory,inventoryToolHolder, 9);
+			}
+			else
+				return null;
+		}
+		else
+			return null;
+	}
+	public World getClientWorld()
+	{
+		return null;
+	}
+}
---
+++ java/ak/MultiToolHolders/InventoryToolHolder.java
@@ -0,0 +1,102 @@
+package ak.MultiToolHolders;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class InventoryToolHolder implements IInventory{
+    public ToolHolderData data;
+
+    public InventoryToolHolder(ItemStack stack, World world) {
+        data = ((ItemMultiToolHolder)stack.getItem()).getData(stack, world);
+    }
+    @Override
+    public int getSizeInventory()
+    {
+        return 9;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int var1)
+    {
+        return data.tools[var1];
+    }
+
+    @Override
+    public ItemStack decrStackSize(int var1, int var2)
+    {
+        if(data.tools[var1] != null)
+        {
+            ItemStack var3;
+            if(data.tools[var1].stackSize <= var2)
+            {
+                var3 = data.tools[var1];
+                data.tools[var1] = null;
+                this.markDirty();
+                return var3;
+            }
+            else
+            {
+                var3 = data.tools[var1].splitStack(var2);
+
+                if (data.tools[var1].stackSize == 0)
+                {
+                    data.tools[var1] = null;
+                }
+
+                this.markDirty();
+                return var3;
+            }
+        }
+        else
+            return null;
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int var1)
+    {
+        return null;
+    }
+
+    @Override
+    public void setInventorySlotContents(int var1, ItemStack var2) {
+        data.tools[var1] = var2;
+    }
+
+    @Override
+    public String getInventoryName() {
+        return "ToolHolder";
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return 1;
+    }
+
+	@Override
+	public void markDirty() {
+		data.upDate = true;
+	}
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer var1) {
+        return true;
+    }
+
+    @Override
+    public void openInventory() {}
+
+    @Override
+    public void closeInventory() {
+        this.markDirty();
+    }
+    @Override
+    public boolean hasCustomInventoryName() {
+        return false;
+    }
+    @Override
+    public boolean isItemValidForSlot(int i, ItemStack itemstack) {
+        return false;
+    }
+}
